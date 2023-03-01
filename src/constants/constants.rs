1d6ac4693bbb47037f014748187229fc067e9ad2
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-03-01 17:54:34 +0100

    Moved database.

--- src/constants/constants.rs
+++ src/constants/constants.rs
@@ -1,6 +1,6 @@
 
 pub static ITUNES_URL: &str = "https://itunes.apple.com/search?term=";
 
-pub static DB_NAME: &str = "podcast.db";
+pub static DB_NAME: &str = "db/podcast.db";
 
 pub static PODCASTS_ROOT_DIRECTORY: &str = "podcasts";
