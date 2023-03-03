d61f13feffec528d545887bd8a13b67e646ef915
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-03-03 21:50:02 +0100

    Removed unused sidebar elements.

--- ui/src/components/SideBar.tsx
+++ ui/src/components/SideBar.tsx
@@ -13,10 +13,6 @@ export const SideBar  = ()=>{
                 <SideBarItem highlightPath={'/home'} translationkey={t('homepage')} icon={<i className="fa-solid fa-house fa-xl"></i>}/>
                 <SideBarItem highlightPath={'/podcasts'} translationkey={t('podcasts')} icon={<i className="fa-solid fa-guitar   fa-xl"></i>
                 }/>
-                <SideBarItem highlightPath={'/folder'} translationkey={t('folderView')} icon={<i className="fa-solid fa-folder fa-xl"></i>}/>
-                <SideBarItem highlightPath={'/notes'} translationkey={t('notesView')} icon={<i className="fa-solid fa-note-sticky fa-xl"></i>}/>
-                <SideBarItem highlightPath={'/concerts'} translationkey={t('concertView')} icon={<i className="fa-solid fa-ticket fa-xl"></i>}/>
-                <SideBarItem highlightPath={'/io'} translationkey={t('io')} icon={<i className="fa-solid fa-download fa-xl"></i>}/>
             </ul>
         </div>
     </aside>
