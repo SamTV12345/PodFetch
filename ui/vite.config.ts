9d84ba90f87209184deb0cdf0fc9f9057e77506f
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-03-03 21:08:27 +0100

    Fixed base path

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/ui/',
-  build: {
-    outDir: '../static',
-  },
   plugins: [react()],
 })
