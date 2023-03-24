2a9f175100f2bb554f48e1cd1b276f3fdc07ca00
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-03-24 20:27:08 +0100

    Fixed database url throwing instead of being set to the default value.

--- src/config/dbconfig.rs
+++ src/config/dbconfig.rs
@@ -5,7 +5,7 @@ use std::env;
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("sqlite::/./db/podcast.db");
+    let database_url = env::var("DATABASE_URL").unwrap_or("sqlite::/./db/podcast.db".to_string());
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
