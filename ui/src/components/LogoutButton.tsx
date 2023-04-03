6f25ff2ffa2dd08da58e7417b7820d12b6b0dae6
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-04-03 19:48:57 +0200

    Add background color.

--- ui/src/components/LogoutButton.tsx
+++ ui/src/components/LogoutButton.tsx
@@ -4,7 +4,7 @@ import {useTranslation} from "react-i18next";
 export const LogoutButton = () => {
     const auth = useAuth()
     const {t} = useTranslation()
-    return <button className="text-white" onClick={()=>{
+    return <button className="text-white bg-blue-600 rounded pt-2 pb-2 w-full hover:bg-blue-500 active:scale-95" onClick={()=>{
         auth.signoutRedirect()
     }}>{t('logout')}</button>
 }
