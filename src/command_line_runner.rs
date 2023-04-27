ab83d103e2a2f8d23bc48a88c8a62c423d6ede01
samuel1998.schwanzer <samuel1998.schwanzer@gmail.com>
2023-04-27 14:47:00 +0200

    Added user management via cli with add, remove,list

--- src/command_line_runner.rs
+++ src/command_line_runner.rs
@@ -1,32 +1,88 @@
 use std::alloc::System;
 use std::env::Args;
-use std::io::{Read, stdin};
+use std::io::{Error, ErrorKind, Read, stdin, stdout, Write};
+use std::process::exit;
 use std::str::FromStr;
+use diesel::SqliteConnection;
 use log::error;
+use sha256::digest;
+use crate::config::dbconfig::establish_connection;
 use crate::constants::constants::Role;
-use crate::models::user::User;
+use crate::models::user::{User, UserWithoutPassword};
 use crate::utils::time::get_current_timestamp_str;
+use rpassword::read_password;
+
 
 pub fn start_command_line(mut args: Args){
     println!("Starting from command line");
-    println!("{:?}", args);
     match args.nth(1).unwrap().as_str() {
+        "help"|"--help"=>{
+            println!(r" The following commands are available:
+            add => Adds a user
+            remove => Removes a user
+            update => Updates a user
+            list => Lists all users
+            ")
+        }
         "users"=>{
             println!("User management");
             match args.nth(0).unwrap().as_str() {
                 "add"=> {
-                    read_user_account();
+                    let mut user = read_user_account();
+
+
+                    println!("Should a user with the following settings be applied {:?}",user);
+
+                    match ask_for_confirmation(){
+                        Ok(e)=>{
+                            user.password = Some(digest(user.password.unwrap()));
+                            match User::insert_user(&mut user, &mut establish_connection()){
+                                Ok(e)=>{
+                                    println!("User succesfully created")
+                                },
+                                Err(..)=>{
+
+                                }
+                            }
+                        },
+                        Err(..)=> {
+                        }
+                    }
                 }
                 "remove"=> {
+                    let mut username = String::new();
                     // remove user
+                    let available_users = list_users();
+                    retry_read("Please enter the username of the user you want to delete",
+                               &mut username);
+                    username = trim_string(username);
+                    println!("{}", username);
+                    match available_users.iter().find(|u|u.username==username){
+                        Some(u)=>{
+                            User::delete_by_username(trim_string(username),
+                                                     &mut establish_connection())
+                                .expect("Error deleting user");
+                        },
+                        None=>{
+                            println!("Username not found")
+                        }
+                    }
+
                 }
                 "list"=> {
                     // list users
+
+                    list_users();
                 }
                 _ => {
                     error!("Command not found")
                 }
             }
+        }
+        "help"|"--help"=>{
+            println!(r" The following commands are available:
+            users => Handles user management
+            ")
         }
         _ => {
             error!("Command not found")
@@ -34,21 +90,35 @@ pub fn start_command_line(mut args: Args){
     }
 }
 
+fn list_users() -> Vec<UserWithoutPassword> {
+    let users = User::find_all_users(&mut establish_connection());
+
+    users.iter().for_each(|u| {
+        println!("|Username|Role|Explicit Consent|Created at|", );
+        println!("|{}|{}|{}|{}|", u.username, u.role, u.explicit_consent, u.created_at);
+    });
+    users
+}
 
 
 pub fn read_user_account()->User{
     let mut username = String::new();
     let mut password = String::new();
-    let mut role_change = Role::User;
 
     let role = Role::VALUES.map(|v|{
         return v.to_string()
     }).join(", ");
     retry_read("Enter your username: ", &mut username);
-    retry_read("Enter your password: ", &mut password);
+
+    let user_exists = User::find_by_username(&username, &mut establish_connection()--h).is_some();
+    if user_exists{
+        println!("User already exists");
+        exit(1);
+    }
+    password = retry_read_secret("Enter your password: ");
     let assigned_role = retry_read_role(&format!("Select your role {}",&role));
 
-    let user = User{
+    let mut user = User{
         id: 0,
         username: username.trim_end_matches("\n").parse().unwrap(),
         role: assigned_role.to_string(),
@@ -56,7 +126,6 @@ pub fn read_user_account()->User{
         explicit_consent: false,
         created_at: get_current_timestamp_str(),
     };
-    println!("{:?}",user);
 
     user
 }
@@ -76,15 +145,51 @@ pub fn retry_read(prompt: &str, input: &mut String){
     }
 }
 
+
+pub fn retry_read_secret(prompt: &str)->String{
+    println!("{}",prompt);
+    stdout().flush().unwrap();
+    let mut input = read_password().unwrap();
+    match  input.len()>0{
+        true => {
+            if input.trim().len()==0{
+                retry_read(prompt, &mut input);
+            }
+        }
+        false => {
+            retry_read(prompt, &mut input);
+        }
+    }
+    input
+}
+
 pub fn retry_read_role(prompt: &str)->Role{
     let mut input = String::new();
     println!("{}",prompt);
     stdin().read_line(&mut input).unwrap();
     let res = Role::from_str(input.as_str().trim_end_matches("\n"));
-    if res.is_err(){
-        println!("Error setting role. Please choose one of the possible roles.");
-        retry_read_role(prompt);
+    match res{
+        Err(..)=> {
+            println!("Error setting role. Please choose one of the possible roles.");
+            return retry_read_role(prompt);
+        }
+        Ok(..)=>{
+            res.unwrap()
+        }
     }
-    res.unwrap()
 }
 
+fn ask_for_confirmation()->Result<(),Error>{
+    let mut input = String::new();
+    println!("Y[es]/N[o]");
+    stdin().read_line(&mut input).expect("Error reading from terminal");
+    match input.to_lowercase().starts_with("y") {
+        true=>Ok(()),
+        false=>Err(Error::new(ErrorKind::WouldBlock, "Interrupted by user."))
+    }
+}
+
+
+fn trim_string(string_to_trim: String)->String{
+    string_to_trim.trim_end_matches("\n").parse().unwrap()
+}
