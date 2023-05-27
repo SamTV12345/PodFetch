522900ec7b8cbc981bc3daf51a89a4f0d4a85c5a
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-05-27 21:02:24 +0200

    Added extern macro for importing the configuration.

--- src/dbconfig/mod.rs
+++ src/dbconfig/mod.rs
@@ -47,3 +47,40 @@ macro_rules! import_database_connections {
         use diesel::MysqlConnection;
     };
 }
+
+
+#[macro_export]
+macro_rules! import_database_config{
+    ()=>{
+    #[cfg(sqlite)]
+    type DbConnection = SqliteConnection;
+    #[cfg(sqlite)]
+    pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations/sqlite");
+    #[cfg(sqlite)]
+    use diesel::sqlite::SqliteQueryBuilder;
+    #[cfg(sqlite)]
+    pub type MyQueryBuilder = SqliteQueryBuilder;
+
+
+    #[cfg(postgresql)]
+    pub type DbConnection = PgConnection;
+
+    #[cfg(postgresql)]
+    use diesel::pg::PgQueryBuilder;
+    #[cfg(postgresql)]
+    pub type MyQueryBuilder = PgQueryBuilder;
+
+    #[cfg(postgresql)]
+    pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations/postgres");
+
+    #[cfg(mysql)]
+    type DbConnection = MysqlConnection;
+    #[cfg(mysql)]
+    pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations/mysql");
+
+    #[cfg(mysql)]
+    use diesel::mysql::MysqlQueryBuilder;
+    #[cfg(mysql)]
+    pub type MyQueryBuilder = MysqlQueryBuilder;
+    }
+}
