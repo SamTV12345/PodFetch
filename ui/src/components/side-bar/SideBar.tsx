3b6f6d586cbe371986a68d1b3679d228b53b2801
doba16 <92383352+doba16@users.noreply.github.com>
2023-05-31 23:38:43 +0200

    Added proper styling of sidebar

--- ui/src/components/side-bar/SideBar.tsx
+++ ui/src/components/side-bar/SideBar.tsx
@@ -1,10 +1,9 @@
 import {useTranslation} from "react-i18next";
 import {SideBarItem} from "./SideBarItem";
 import {useAppSelector} from "../../store/hooks";
-import {PodFetchLogo} from "../../icons/PodFetchLogo";
-import {HeartIcon} from "../../icons/HeartIcon";
-import {PodcastIcon} from "../../icons/PodcastIcon";
+import {PodFetchIcon} from "../../icons/PodFetchIcon";
 import "./style.scss"
+import {MdFavoriteBorder, MdOutlineHome, MdOutlineSettings, MdPodcasts, MdTimeline} from "react-icons/md";
 
 export const SideBar = () => {
     const sideBarCollapsed = useAppSelector(state => state.common.sideBarCollapsed)
@@ -14,29 +13,30 @@ export const SideBar = () => {
 
     return (
         <aside className={`side-bar ${sideBarCollapsed ? 'closed' : 'open'}`} aria-label="Sidebar">
-            <div className="py-4 px-3 bg-gray-950 w-full">
-                <div className="text-amber-600 flex text-2xl gap-2"><PodFetchLogo/><h1
-                    className="text-white pt-1">Podfetch</h1></div>
-                <ul className="pt-20">
-                    <SideBarItem highlightPath={'./'} translationkey={t('homepage')}
-                                 icon={<i className="fa-solid fa-house fa-xl"></i>}/>
-                    <SideBarItem highlightPath={'podcasts'} translationkey={t('all-subscriptions')}
-                                 icon={<PodcastIcon/>}/>
-                    <SideBarItem highlightPath={"favorites"} translationkey={t('favorites')} icon={<HeartIcon/>}/>
-                    <SideBarItem highlightPath={"timeline"} translationkey={t('timeline')}
-                                 icon={<i className="fa-solid fa-timeline fa-xl"/>}/>
-                    {/*<SideBarItem highlightPath={"info"} translationkey={t('info')} icon={<i className="fa-solid fa-info-circle fa-xl"></i>}/>*/}
-                    {<div className="display-only-mobile"><SideBarItem highlightPath={"/podcasts/search"}
-                                                                       translationkey={t('search-podcasts')}
-                                                                       icon={<i className="fa-solid fa-search"/>}/>
-                    </div>}
-                    {(config?.oidcConfig || config?.basicAuth) &&
-                        <SideBarItem highlightPath={"administration"} translationkey={t('administration')}
-                                     icon={<i className="fa-solid fa-gavel fa-xl"/>}/>}
-                    <SideBarItem className="absolute bottom-0 w-11/12" highlightPath={"settings"}
-                                 translationkey={t('settings')} icon={<i className="fa-solid fa-gear fa-xl"/>}/>
-                </ul>
+            <div className="logo-section">
+                <PodFetchIcon/>
+                <h1 className="text-white pt-1">Podfetch</h1>
             </div>
+
+            <ul className="navigation-section">
+                <SideBarItem highlightPath={'./'} translationKey={t('homepage')}
+                             icon={<MdOutlineHome />}/>
+                <SideBarItem highlightPath={'podcasts'} translationKey={t('all-subscriptions')}
+                             icon={<MdPodcasts/>}/>
+                <SideBarItem highlightPath={"favorites"} translationKey={t('favorites')} icon={<MdFavoriteBorder/>}/>
+                <SideBarItem highlightPath={"timeline"} translationKey={t('timeline')}
+                             icon={<MdTimeline/>}/>
+                {/*<SideBarItem highlightPath={"info"} translationkey={t('info')} icon={<i className="fa-solid fa-info-circle fa-xl"></i>}/>*/}
+                {<div className="display-only-mobile"><SideBarItem highlightPath={"/podcasts/search"}
+                                                                   translationKey={t('search-podcasts')}
+                                                                   icon={<i className="fa-solid fa-search"/>}/>
+                </div>}
+                {(config?.oidcConfig || config?.basicAuth) &&
+                    <SideBarItem highlightPath={"administration"} translationKey={t('administration')}
+                                 icon={<i className="fa-solid fa-gavel fa-xl"/>}/>}
+                <SideBarItem highlightPath={"settings"} spaceBefore
+                             translationKey={t('settings')} icon={<MdOutlineSettings/>}/>
+            </ul>
         </aside>
     )
 }
--- ui/src/components/side-bar/SideBarItem.tsx
+++ ui/src/components/side-bar/SideBarItem.tsx
@@ -4,24 +4,26 @@ import {useAppDispatch} from "../../store/hooks";
 import {setSideBarCollapsed} from "../../store/CommonSlice";
 
 type SideBarItemProps = {
-    highlightPath:string,
-    translationkey: string,
-    icon:React.ReactElement,
-    className?:string
+    highlightPath: string,
+    translationKey: string,
+    icon: React.ReactElement,
+    spaceBefore?: boolean
 }
 
-export const SideBarItem:FC<SideBarItemProps>  =({highlightPath,translationkey,icon, className})=>{
+export const SideBarItem: FC<SideBarItemProps> = ({highlightPath, translationKey, icon, spaceBefore}) => {
     const dispatch = useAppDispatch()
 
-    const minimizeOnMobile = ()=>{
-        if(window.screen.width<768){
+    const minimizeOnMobile = () => {
+        if (window.screen.width < 768) {
             dispatch(setSideBarCollapsed(true))
         }
     }
-    return   <li className={"sidebar "+className} onClick={()=>minimizeOnMobile()}>
-        <NavLink to={highlightPath} className="flex pl-2  mt-1 mb-1  items-center text-base font-normal rounded-lg text-white hover:text-amber-400 hover:bg-amber-400 hover:bg-opacity-30 h-14">
-            {icon}
-            <span className="ml-3 hover:text-inherit">{translationkey}</span>
-        </NavLink>
-    </li>
+    return (
+        <li onClick={() => minimizeOnMobile()} className={spaceBefore ? "space-before" : ""}>
+            <NavLink to={highlightPath}>
+                {icon}
+                <span>{translationKey}</span>
+            </NavLink>
+        </li>
+    )
 }
