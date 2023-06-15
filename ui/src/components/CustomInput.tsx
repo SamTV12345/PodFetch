ef3cfcf547a4f56a675070311d9c85014012d535
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2023-06-14 23:58:57 -0700

    Rename and remove width from input component

--- ui/src/components/CustomInput.tsx
+++ ui/src/components/CustomInput.tsx
@@ -1,15 +1,15 @@
 import {ChangeEventHandler, FC} from "react"
 
-type InputProps = {
+type CustomInputProps = {
   className?: string,
   type?: string,
   placeholder?: string,
   name?: string,
-  value?: string,
+  value?: string | number,
   onChange?: ChangeEventHandler<HTMLInputElement>
 }
 
-export const Input:FC<InputProps> = ({className = '', type = 'text', placeholder, name, value, onChange}) => {
+export const CustomInput:FC<CustomInputProps> = ({className = '', type = 'text', placeholder, name, value, onChange}) => {
   return <input type={type} placeholder={placeholder} name={name} value={value} onChange={onChange}
-  className={"bg-stone-100 w-full px-4 py-2 rounded-lg text-sm text-stone-600 " + className}/>
+  className={"bg-stone-100 px-4 py-2 rounded-lg text-sm text-stone-600 " + className}/>
 }
