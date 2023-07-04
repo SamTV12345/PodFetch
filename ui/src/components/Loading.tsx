b1fae27ce910fbd3f0a86f766db666caefae5ff1
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2023-07-03 22:17:44 -0700

    Redesign loading component

--- ui/src/components/Loading.tsx
+++ ui/src/components/Loading.tsx
@@ -3,28 +3,14 @@ import {Spinner} from "./Spinner";
 
 export const Loading = () => {
     const {t} = useTranslation()
-    return <div className=" grid w-full h-full place-items-center">
-        <div role="status" className="border-2 w-full p-2 rounded bg-gray-800 md:w-auto">
-            <div className="flex gap-2">
+
+    return (
+        <div className="grid place-items-center h-full w-full">
+            <div className="flex items-center gap-3" role="status">
                 <Spinner/>
-                <div>
-                    <div className="flex  align-baseline">
-                        <div className="mt-1 text-white">{t('loading')}</div>
-                        <div className="flex dots-distance ml-1 dots-margin-bottom">
-                            <div className="flex flex-col-reverse">
-                                <div className="bg-slate-500  p-1 w-1 h-1 rounded-full animate-bounce bottom-0"></div>
-                            </div>
-                            <div className="flex flex-col-reverse">
-                                <div className="bg-slate-600 p-1 w-1 h-1 rounded-full animate-bounce"></div>
-                            </div>
-                            <div className="flex flex-col-reverse">
-                            <div className="bg-slate-700  p-1 w-1 h-1 rounded-full animate-bounce"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                <span className="text-stone-900">{t('loading')}...</span>
             </div>
-            <span className="sr-only">Loading...</span>
         </div>
-    </div>
+    )
 }
