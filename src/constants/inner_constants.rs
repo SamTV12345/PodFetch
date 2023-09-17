d01ae227b255d663f1bb75f9d4f7225bb9276a44
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-09-17 14:58:28 +0200

    Fixed request when getting the podcast feed and feed provider requires User Agent.

--- src/constants/inner_constants.rs
+++ src/constants/inner_constants.rs
@@ -131,3 +131,7 @@ pub const JS: &str = "javascript";
 
 
 pub const MAX_FILE_TREE_DEPTH:i32 = 4;
+
+
+pub const COMMON_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36";
