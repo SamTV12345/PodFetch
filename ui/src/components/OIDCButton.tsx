32be4b3242b6a1db02ca4eefdf4fd1d69d6e923a
SamTV12345 <40429738+SamTV12345@users.noreply.github.com>
2023-11-05 18:37:50 +0100

    Added logout button and login name. (#386)
    * Added logout button and login name.
    
    * Added option for adapting text correctly.

--- ui/src/components/OIDCButton.tsx
+++ ui/src/components/OIDCButton.tsx
@@ -3,14 +3,17 @@ import { useAuth } from 'react-oidc-context'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { CustomButtonPrimary } from './CustomButtonPrimary'
+import useCommon from "../store/CommonSlice";
 
 export const OIDCButton = () => {
     const auth = useAuth()
     const navigate = useNavigate()
     const { t } = useTranslation()
+    const setLoginData = useCommon(state => state.setLoginData)
 
     if (auth.isAuthenticated && auth.user) {
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + auth.user.id_token;
+        setLoginData({username: auth.user.profile.preferred_username, password: ''})
         navigate('/')
     }
 
--- ui/src/components/UserMenu.tsx
+++ ui/src/components/UserMenu.tsx
@@ -1,50 +1,69 @@
-import { FC } from 'react'
+import {FC, useMemo} from 'react'
 import { useAuth } from 'react-oidc-context'
 import { CustomDropdownMenu } from './CustomDropdownMenu'
 import { MenuItem } from './CustomDropdownMenu'
 import 'material-symbols/outlined.css'
 import useCommon from "../store/CommonSlice";
 
+
+const AccountTrigger = ()=>{
+    const username = useCommon(state => state.loginData)
+
+    return <button className="flex gap-3"><span className="hidden md:block text-[--fg-color]">{username?.username}</span><span className="material-symbols-outlined text-[--fg-color] hover:text-[--fg-color-hover]">account_circle</span></button>
+}
+
 export const UserMenu: FC = () => {
     const config = useCommon(state => state.configModel)
     const configModel = useCommon(state => state.configModel)
+    const menuItems: Array<MenuItem> = useMemo(()=>{
+        const menuItems: Array<MenuItem> = [
+            {
+                iconName: 'settings',
+                translationKey: 'settings',
+                path: 'settings'
+            },
+            {
+                iconName: 'info',
+                translationKey: 'system-info',
+                path: 'info'
+            }
+        ]
 
-    const menuItems: Array<MenuItem> = [
-        {
-            iconName: 'settings',
-            translationKey: 'settings',
-            path: 'settings'
-        },
-        {
-            iconName: 'info',
-            translationKey: 'system-info',
-            path: 'info'
+        if (config?.oidcConfig || config?.basicAuth) {
+            menuItems.push({
+                iconName: 'group',
+                translationKey: 'administration',
+                path: 'administration'
+            })
         }
-    ]
 
-    if (config?.oidcConfig || config?.basicAuth) {
-        menuItems.push({
-            iconName: 'group',
-            translationKey: 'administration',
-            path: 'administration'
-        })
-    }
+        if (configModel?.oidcConfigured) {
+            const auth = useAuth()
 
-    if (configModel?.oidcConfigured) {
-        const auth = useAuth()
+            menuItems.push({
+                iconName: 'logout',
+                translationKey: 'logout',
+                onClick: () => auth.signoutRedirect()
+            })
+        }
 
-        menuItems.push({
-            iconName: 'logout',
-            translationKey: 'logout',
-            onClick: () => auth.signoutRedirect()
-        })
-    }
+        if(configModel?.basicAuth){
+            menuItems.push({
+                iconName: 'logout',
+                translationKey: 'logout',
+                onClick: () => {
+                    localStorage.removeItem('auth')
+                    sessionStorage.removeItem('auth')
+                    window.location.reload()
+                }
+            })
+        }
 
-    const trigger = () => (
-        <span className="material-symbols-outlined text-[--fg-color] hover:text-[--fg-color-hover]">account_circle</span>
-    )
+        return menuItems
+    }, [configModel,config])
+
 
     return (
-        <CustomDropdownMenu menuItems={menuItems} trigger={trigger()} />
+        <CustomDropdownMenu menuItems={menuItems} trigger={<AccountTrigger/>} />
     )
 }
