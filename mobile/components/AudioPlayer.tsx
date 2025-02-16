c8c88767663ff12d0734e25856b88053ce901dcb
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-02-16 21:05:57 +0100

    Added playback of audio

--- mobile/components/AudioPlayer.tsx
+++ mobile/components/AudioPlayer.tsx
@@ -7,9 +7,13 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export const AudioPlayer = ()=>{
     const selectedPodcastEpisode = useStore(state=>state.podcastEpisodeRecord)
+    const changeAudioState = useStore(state=>state.togglePlaying)
+    const playingAudio = useStore(state=>state.isPlaying)
+    const audioPercent = useStore(state=>state.audioProgress)
 
-    return <View style={{bottom: 10, marginLeft: 18, marginRight: 18, backgroundColor: styles.darkColor, display: 'flex', flexDirection: 'row', position: 'relative'}}>
-        <View style={{width: '60%', backgroundColor: 'white', height: 2, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20}}></View>
+
+    return selectedPodcastEpisode?<View style={{bottom: 10, marginLeft: 18, marginRight: 18, backgroundColor: styles.darkColor, display: 'flex', flexDirection: 'row', position: 'relative'}}>
+        <View style={{width: `${audioPercent}%`, backgroundColor: 'white', height: 2, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20}}></View>
         <View style={{width: '100%', backgroundColor: styles.gray, height: 2, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20}}></View>
         <Image src={selectedPodcastEpisode?.podcastEpisode.local_image_url} style={{width: 40, height: 40,alignSelf: 'center', marginLeft: 10, marginTop: 10}}/>
         <View style={{flexDirection: 'column', marginTop: 10, width: '90%'}}>
@@ -20,8 +24,8 @@ export const AudioPlayer = ()=>{
         </View>
         <View style={{position: 'absolute', right: 10, alignSelf: 'center', paddingTop: 10, display: 'flex', flexDirection: 'row', gap: 3}}>
             <MaterialIcons name="ios-share" color="white" size={20}/>
-            <AntDesign size={20} name="caretright" color="white"/>
+            {playingAudio ? <AntDesign name="pause" size={20} color="white" onPress={()=>changeAudioState()}/>:  <AntDesign size={20} name="caretright" color="white" onPress={()=>changeAudioState()}/>}
         </View>
-    </View>
+    </View>: <View></View>
 
 }
--- mobile/store/store.ts
+++ mobile/store/store.ts
@@ -1,10 +1,52 @@
 import { create } from 'zustand'
 import {components} from "@/schema";
+import {Audio} from 'expo-av'
+import {SoundObject} from "expo-av/build/Audio/Sound";
+
 
 type ZustandStore = {
-    podcastEpisodeRecord: components["schemas"]["PodcastWatchedEpisodeModelWithPodcastEpisode"]|undefined
+    podcastEpisodeRecord: components["schemas"]["PodcastWatchedEpisodeModelWithPodcastEpisode"]|undefined,
+    setPodcastEpisodeRecord: (comp: components["schemas"]["PodcastWatchedEpisodeModelWithPodcastEpisode"])=>void,
+    audioSource: SoundObject|undefined,
+    isPlaying: boolean,
+    togglePlaying: ()=>void,
+    audioProgress: number
 }
 
 export const useStore = create<ZustandStore>((set, get)=>({
-    podcastEpisodeRecord: undefined
+    podcastEpisodeRecord: undefined,
+    setPodcastEpisodeRecord: (e)=>{
+        set({
+            podcastEpisodeRecord: e
+        })
+        Audio.Sound.createAsync({uri: e.podcastEpisode.local_url}, {shouldPlay: true}, (status)=>{
+
+        }).then(audio=>{
+            audio.sound!.setOnPlaybackStatusUpdate((status)=>{
+                if (status.isLoaded) {
+                    const status2 = status.positionMillis/status.durationMillis!
+                    useStore.setState({
+                        audioProgress: status2 * 100
+                    })
+                }
+            })
+            set({audioSource: audio})
+            get().togglePlaying()
+        }).catch(e=>{
+            console.log(e)
+        })
+    },
+    audioSource: undefined,
+    isPlaying: false,
+    togglePlaying:()=>{
+        const playing = !get().isPlaying
+        const audioSource = get().audioSource
+        set({isPlaying: playing})
+        if (playing && audioSource) {
+            audioSource.sound.playAsync()
+        } else if (!playing && audioSource) {
+            audioSource.sound.pauseAsync()
+        }
+    },
+    audioProgress: 0
 }))
