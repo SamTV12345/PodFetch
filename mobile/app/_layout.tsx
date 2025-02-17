ac118c1f7b242996dd95037e80885e63e3325062
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-02-17 22:10:57 +0100

    Added detail info view

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name="podcasts/[id]/info" options={{headerShown: false}} />
         <Stack.Screen name="podcasts/[id]/details" options={{headerShown: false}} />
       </Stack>
       <StatusBar style="light" backgroundColor="#000" />
