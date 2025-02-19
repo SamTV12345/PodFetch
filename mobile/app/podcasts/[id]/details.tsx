d628c8b9480cc1831ea7043e6bbb2c3c81ca6951
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-02-19 21:26:42 +0100

    Continued with podcast detail page

--- mobile/app/podcasts/[id]/details.tsx
+++ mobile/app/podcasts/[id]/details.tsx
@@ -1,14 +1,24 @@
-import {Image, Text, View} from "react-native";
+import {Image, ScrollView, Text, View} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {Link, useLocalSearchParams} from "expo-router";
 import {$api} from "@/client";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import Heading2 from "@/components/text/Heading2";
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import {PodcastEpisodeSlide} from "@/components/PodcastEpisodeSlide";
+import { router } from 'expo-router';
 
 export default function () {
     const { id } = useLocalSearchParams();
-    const {data, isLoading, error} = $api.useQuery('get', '/api/v1/podcasts/{id}', {
+    const {data, isLoading} = $api.useQuery('get', '/api/v1/podcasts/{id}', {
+        params: {
+            path: {
+                id: id as string
+            }
+        }
+    })
+    const dataEpisodes = $api.useQuery('get', '/api/v1/podcasts/{id}/episodes', {
         params: {
             path: {
                 id: id as string
@@ -17,11 +27,14 @@ export default function () {
     })
 
     return <SafeAreaView>
-        <MaterialIcons size={40} color="white" name="chevron-left" style={{
-            position: 'absolute',
-            top: 40,
-            left: 20,
-        }} />
+        <ScrollView overScrollMode="never">
+                <MaterialIcons size={40} color="white" name="chevron-left" style={{
+                    position: 'absolute',
+                    top: 20,
+                    left: 20,
+                }} onPress={()=>{
+                    router.push('/(tabs)')
+                }} />
         {
             !isLoading && data? <>
                 <Image src={data.image_url}   style={{
@@ -39,15 +52,25 @@ export default function () {
                 />
                 <Heading2 styles={{marginRight: 'auto', marginLeft: 'auto', width: '95%', marginTop: 10, paddingBottom: 0}}>{data.name}</Heading2>
                 {data.tags.map(t=><Text>{t.name}</Text>)}
-                <View style={{marginLeft: 30}}>
+                <View style={{marginLeft: 30, display: 'flex', flexDirection: 'row', gap: 10}}>
+                    <AntDesign name="hearto" size={24} color="white" />
                     <Link href={{pathname: "/podcasts/[id]/info", params: {
                                 id: data.id
                             }}}>
                         <AntDesign name="infocirlce" size={24} color="white" />
                     </Link>
+                    <Entypo name="share-alternative" size={24} color="white" />
+                </View>
+                <View style={{margin: 20}}>
+                    {
+                        !dataEpisodes.isLoading && dataEpisodes.data!.map(d=>{
+                            return <PodcastEpisodeSlide episode={d} key={d.podcastEpisode.id}/>
+                        })
+                    }
                 </View>
             </>: <>
             </>
         }
+        </ScrollView>
     </SafeAreaView>
 }
