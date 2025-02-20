820bfb832136d2694802b636efa0594402ba6789
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-02-20 18:46:23 +0100

    Added podcast slide

--- mobile/components/PodcastEpisodeSlide.tsx
+++ mobile/components/PodcastEpisodeSlide.tsx
@@ -3,6 +3,9 @@ import {Image, Text, View} from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {styles} from "@/styles/styles";
 import Svg, {Circle} from "react-native-svg";
+import {useMemo} from "react";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import {useTranslation} from "react-i18next";
 
 type PodcastEpisodeSlideProps = {
     episode: components["schemas"]["PodcastEpisodeWithHistory"],
@@ -15,8 +18,8 @@ const ProgressCircle = ({ progress = 50 }) => {
     const progressStroke = (progress / 100) * circumference;
 
     return (
-        <View>
-            <Svg height={35} width={35} viewBox="0 0 40 40">
+        <View style={{position: 'relative', height: 35, width: 35}} >
+            <Svg viewBox="0 0 40 40" style={{position: 'absolute', left: 0, top: 0}}>
                 {/* Progress Circle */}
                 <Circle
                     cx="20"
@@ -31,7 +34,7 @@ const ProgressCircle = ({ progress = 50 }) => {
                     transform="rotate(-90 20 20)"
                 />
             </Svg>
-            <View style={{backgroundColor: styles.lightgray, height: 25, width: 25, borderRadius: 50, position: 'absolute'}}>
+            <View style={{backgroundColor: styles.lightgray, left: 5, top: 5, height: 25, width: 25, borderRadius: 50, position: 'absolute'}}>
                 <FontAwesome name="play" color="white" style={{margin: 'auto'}}/>
             </View>
         </View>
@@ -39,15 +42,44 @@ const ProgressCircle = ({ progress = 50 }) => {
 };
 
 export const PodcastEpisodeSlide = ({ episode }: PodcastEpisodeSlideProps) => {
+    const totalProgressPercentage = useMemo(()=>{
+        return (episode.podcastHistoryItem?.position||0)/(episode.podcastHistoryItem?.total||1)*100
+    }, [episode])
+    const {t} = useTranslation()
+    const remainingSeconds = useMemo(()=>{
+        const isUnPlayed = episode.podcastHistoryItem?.position === undefined
+        const isCompletelyPlayed = episode.podcastHistoryItem?.position === episode.podcastHistoryItem?.total
+        if(isUnPlayed || isCompletelyPlayed){
+            return {
+                time: Math.floor(episode.podcastEpisode.total_time/60),
+                alreadyPlaying: false
+            }
+        }
 
-    return <View>
-        <View  style={{display: 'flex', flexDirection: 'row', marginTop: 10, marginBottom: 10}}>
-            <Image style={{ width: 50, height: 50, borderRadius: 5 }}
-                             src={episode.podcastEpisode.local_image_url} />
-            <Text style={{ color: 'white', wordWrap: "break", marginLeft: 10 }} numberOfLines={2}>{episode.podcastEpisode.name}</Text>
-        </View>
-        <View>
-            <ProgressCircle progress={(episode.podcastHistoryItem?.position||0)/(episode.podcastHistoryItem?.total||1)} />
+        return {
+            time: Math.floor(((episode.podcastHistoryItem?.total|| episode.podcastEpisode.total_time) - (episode.podcastHistoryItem?.position||0))/60),
+            alreadyPlaying: episode.podcastHistoryItem?.position != undefined && episode.podcastHistoryItem?.position != episode.podcastHistoryItem?.total
+        }
+    } ,[episode])
+
+    return <View style={{marginBottom: 10}}>
+        <View  style={{display: 'flex', flexDirection: 'row', marginTop: 10}}>
+            <View>
+                <View  style={{position: 'relative'}}>
+                    <Image style={{ width: 50, height: 50, borderRadius: 5 }}
+                           src={episode.podcastEpisode.local_image_url} />
+                    {totalProgressPercentage == 100 && <AntDesign name="checkcircle" size={24} color="white" style={{position: 'absolute', bottom: 1}} />}
+                </View>
+            </View>
+            <View style={{width: '80%'}}>
+                <Text style={{ color: 'white', wordWrap: "break", marginLeft: 10 }} numberOfLines={2}>{episode.podcastEpisode.name}</Text>
+                <Text style={{ color: styles.whiteSubText, wordWrap: "break", marginLeft: 10 }} numberOfLines={2}>{episode.podcastEpisode.description}</Text>
+                <View style={{marginLeft: 10, marginTop: 'auto', display: 'flex', flexDirection: 'row'}}>
+                    <ProgressCircle progress={totalProgressPercentage} />
+                    {remainingSeconds.alreadyPlaying ? <Text style={{color: 'white', marginTop: 'auto', marginBottom: 'auto'}}>{t('time-left', {time: remainingSeconds.time})}</Text>:
+                        <Text style={{color: 'white', marginTop: 'auto', marginBottom: 'auto'}}>{t('time', {time: remainingSeconds.time})}</Text>}
+                </View>
+            </View>
         </View>
     </View>
 }
