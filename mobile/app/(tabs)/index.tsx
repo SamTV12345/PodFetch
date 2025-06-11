d18ef30f51ae66ea79aa166be53a9721ef306a48
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-06-11 12:16:52 +0200

    continued...

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ThemedView } from '@/components/ThemedView';
+import {ThemedView} from '@/components/ThemedView';
 import {SafeAreaView} from "react-native-safe-area-context";
 import {styles} from "@/styles/styles";
 import Heading1 from "@/components/text/Heading1";
@@ -10,60 +10,65 @@ import {LoadingSkeleton} from "@/components/ui/LoadingSkeleton";
 import {PodcastCard} from "@/components/PodcastCard";
 import {PodcastEpisodeCard} from "@/components/PodcastEpisodeCard";
 
-const HomeScreen = ()=> {
-  const {data, isError, isLoading, error} = $api.useQuery('get', '/api/v1/podcasts')
-  const lastWatchedData = $api.useQuery('get', '/api/v1/podcasts/episode/lastwatched')
+const HomeScreen = () => {
+    const {data, isLoading} = $api.useQuery('get', '/api/v1/podcasts')
+    const lastWatchedData = $api.useQuery('get', '/api/v1/podcasts/episode/lastwatched')
     const {t} = useTranslation()
 
-  return (
-    <SafeAreaView>
-      <ThemedView style={{
-        backgroundColor: styles.lightDarkColor,
-          paddingTop: 20,
-          paddingLeft: 10,
-          paddingRight: 10
-      }}>
-          <Heading1>{t('home')}</Heading1>
+    return (
+        <SafeAreaView>
+            <ThemedView style={{
+                backgroundColor: styles.lightDarkColor,
+                paddingTop: 20,
+                paddingLeft: 10,
+                paddingRight: 10
+            }}>
+                <Heading1>{t('home')}</Heading1>
 
-          <Heading2>{t('last-listened')}</Heading2>
-          <ScrollView horizontal={true} style={{paddingBottom: 20, display: 'flex', gap: 10}} overScrollMode="never">
-              {lastWatchedData.isLoading &&
-                  <>
-                  <LoadingSkeleton/>
-              <LoadingSkeleton/>
-              <LoadingSkeleton/>
-              <LoadingSkeleton/>
-              <LoadingSkeleton/>
-              <LoadingSkeleton/>
-              </>
-              }
-              <View style={{display: 'flex', gap: 10, flexDirection: 'row'}}>
-              {
-                  lastWatchedData.data && lastWatchedData.data.map(d=>{
-                      return <PodcastEpisodeCard podcastEpisode={d} key={d.id} />
-                  })
-              }
-              </View>
-          </ScrollView>
+                <Heading2>{t('last-listened')}</Heading2>
+                <ScrollView horizontal={true} style={{paddingBottom: 20, display: 'flex', gap: 10}}
+                            overScrollMode="never">
+                    {lastWatchedData.isLoading &&
+                        <>
+                            <LoadingSkeleton/>
+                            <LoadingSkeleton/>
+                            <LoadingSkeleton/>
+                            <LoadingSkeleton/>
+                            <LoadingSkeleton/>
+                            <LoadingSkeleton/>
+                        </>
+                    }
+                    <View style={{display: 'flex', gap: 10, flexDirection: 'row'}}>
+                        {
+                            lastWatchedData.data && lastWatchedData.data.map(d => {
+                                return <PodcastEpisodeCard podcastEpisode={d} key={d.id}/>
+                            })
+                        }
+                    </View>
+                </ScrollView>
 
 
-          <Heading2 more onMore={()=>{}}>{t('your-podcasts')}</Heading2>
-          <ScrollView horizontal={true} style={{paddingBottom: 20}} overScrollMode="never">
-              {isLoading &&<><LoadingSkeleton/>
-                  <LoadingSkeleton/>
-                  <LoadingSkeleton/>
-                  <LoadingSkeleton/>
-                  <LoadingSkeleton/>
-                  <LoadingSkeleton/></>}
-              {
-                  data && data.map(d=>{
-                      return <PodcastCard podcast={d} key={d.id} />
-                  })
-              }
-          </ScrollView>
-      </ThemedView>
-    </SafeAreaView>
-  );
+                <Heading2 more onMore={() => {
+                }}>{t('your-podcasts')}</Heading2>
+                <ScrollView horizontal={true} style={{paddingBottom: 20, display: 'flex', gap: 10}}
+                            overScrollMode="never">
+                    {isLoading && <><LoadingSkeleton/>
+                        <LoadingSkeleton/>
+                        <LoadingSkeleton/>
+                        <LoadingSkeleton/>
+                        <LoadingSkeleton/>
+                        <LoadingSkeleton/></>}
+                    <View style={{display: 'flex', gap: 10, flexDirection: 'row'}}>
+                        {
+                            data && data.map(d => {
+                                return <PodcastCard podcast={d} key={d.id}/>
+                            })
+                        }
+                    </View>
+                </ScrollView>
+            </ThemedView>
+        </SafeAreaView>
+    );
 }
 
 export default HomeScreen;
--- mobile/components/PodcastEpisodeSlide.tsx
+++ mobile/components/PodcastEpisodeSlide.tsx
@@ -3,25 +3,42 @@ import {Image, Pressable, Text, View} from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {styles} from "@/styles/styles";
 import Svg, {Circle} from "react-native-svg";
-import {useMemo} from "react";
+import {useCallback, useEffect, useMemo, useRef} from "react";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import {useTranslation} from "react-i18next";
-import { router } from 'expo-router';
+import {router} from 'expo-router';
+import {$api} from "@/client";
+import {createAudioPlayer} from 'expo-audio';
 
 type PodcastEpisodeSlideProps = {
     episode: components["schemas"]["PodcastEpisodeWithHistory"],
 }
 
-const ProgressCircle = ({ progress = 50 }) => {
-    const radius = 15; // Radius of the circle
+type ProgressCircleProps = {
+    episode: components["schemas"]["PodcastEpisodeWithHistory"]
+    progress?: number,
+    onPlayPress: () => void,
+}
+
+type PlaybackStatus = {
+    isLoaded: boolean;
+    error?: string;
+    isPlaying?: boolean;
+    isBuffering?: boolean;
+    didJustFinish?: boolean;
+    positionMillis?: number;
+    durationMillis?: number;
+}
+
+const ProgressCircle = ({progress = 50, episode, onPlayPress}: ProgressCircleProps) => {
+    const radius = 15;
     const strokeWidth = 1;
     const circumference = 2 * Math.PI * radius;
     const progressStroke = (progress / 100) * circumference;
 
     return (
-        <View style={{position: 'relative', height: 35, width: 35}} >
+        <View style={{position: 'relative', height: 35, width: 35}}>
             <Svg viewBox="0 0 40 40" style={{position: 'absolute', left: 0, top: 0}}>
-                {/* Progress Circle */}
                 <Circle
                     cx="20"
                     cy="20"
@@ -35,63 +52,143 @@ const ProgressCircle = ({ progress = 50 }) => {
                     transform="rotate(-90 20 20)"
                 />
             </Svg>
-            <View style={{backgroundColor: styles.lightgray, left: 5, top: 5, height: 25, width: 25, borderRadius: 50, position: 'absolute'}}>
-                <FontAwesome name="play" color="white" style={{margin: 'auto'}}/>
-            </View>
+            <Pressable
+                style={{
+                    backgroundColor: styles.lightgray,
+                    left: 5,
+                    top: 5,
+                    height: 25,
+                    width: 25,
+                    borderRadius: 50,
+                    position: 'absolute',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                onPress={onPlayPress}
+            >
+                <FontAwesome name="play" color="white"/>
+            </Pressable>
         </View>
     );
 };
 
-export const PodcastEpisodeSlide = ({ episode }: PodcastEpisodeSlideProps) => {
-    const totalProgressPercentage = useMemo(()=>{
-        return (episode.podcastHistoryItem?.position||0)/(episode.podcastHistoryItem?.total||1)*100
-    }, [episode])
-    const {t} = useTranslation()
+export const PodcastEpisodeSlide = ({episode}: PodcastEpisodeSlideProps) => {
+    const {t} = useTranslation();
 
+    const totalProgressPercentage = useMemo(() => {
+        return ((episode.podcastHistoryItem?.position || 0) /
+            (episode.podcastHistoryItem?.total || 1)) * 100;
+    }, [episode]);
 
-    const remainingSeconds = useMemo(()=>{
-        const isUnPlayed = episode.podcastHistoryItem?.position === undefined
-        const isCompletelyPlayed = episode.podcastHistoryItem?.position === episode.podcastHistoryItem?.total
-        if(isUnPlayed || isCompletelyPlayed){
-            return {
-                time: Math.floor(episode.podcastEpisode.total_time/60),
-                alreadyPlaying: false
-            }
-        }
+    const remainingSeconds = useMemo(() => {
+        const position = episode.podcastHistoryItem?.position;
+        const total = episode.podcastHistoryItem?.total || episode.podcastEpisode.total_time;
+        const isUnPlayed = position === undefined;
+        const isCompletelyPlayed = position === total;
 
         return {
-            time: Math.floor(((episode.podcastHistoryItem?.total|| episode.podcastEpisode.total_time) - (episode.podcastHistoryItem?.position||0))/60),
-            alreadyPlaying: episode.podcastHistoryItem?.position != undefined && episode.podcastHistoryItem?.position != episode.podcastHistoryItem?.total
-        }
-    } ,[episode])
+            time: Math.floor(
+                (isUnPlayed || isCompletelyPlayed
+                        ? episode.podcastEpisode.total_time
+                        : total - (position || 0)
+                ) / 60
+            ),
+            alreadyPlaying: position !== undefined && position !== total
+        };
+    }, [episode]);
 
-    return <View style={{marginBottom: 10}}>
-        <View  style={{display: 'flex', flexDirection: 'row', marginTop: 10}}>
-            <View>
-                <View  style={{position: 'relative'}}>
-                    <Image style={{ width: 50, height: 50, borderRadius: 5 }}
-                           src={episode.podcastEpisode.local_image_url} />
-                    {totalProgressPercentage == 100 && <AntDesign name="checkcircle" size={24} color="white" style={{position: 'absolute', bottom: 1}} />}
+    const episodeQuery = $api.useQuery("get", "/api/v1/podcasts/episode/{id}", {
+        params: {
+            path: {
+                id: episode.podcastEpisode.episode_id
+            }
+        },
+        enabled: false
+    });
+
+    const handlePlaybackStatus = useCallback((status: PlaybackStatus) => {
+        if (!status.isLoaded) {
+            if (status.error) {
+                console.error(`Playback error: ${status.error}`);
+            }
+            return;
+        }
+
+        // Hier können Sie den Wiedergabestatus in Ihrem globalen Zustand aktualisieren
+    }, []);
+
+    const handlePlay = useCallback(async () => {
+        const result = await episodeQuery.refetch();
+        if (result.isSuccess) {
+            const player = createAudioPlayer(episode.podcastEpisode.local_url);
+            player.play();
+            player.addListener('playbackStatusUpdate', handlePlaybackStatus);
+        }
+    }, [episode, episodeQuery]);
+
+    return (
+        <View style={{marginBottom: 10}}>
+            <View style={{display: 'flex', flexDirection: 'row', marginTop: 10}}>
+                <View>
+                    <View style={{position: 'relative'}}>
+                        <Image
+                            style={{width: 50, height: 50, borderRadius: 5}}
+                            source={{uri: episode.podcastEpisode.local_image_url}}
+                        />
+                        {totalProgressPercentage === 100 && (
+                            <AntDesign
+                                name="checkcircle"
+                                size={24}
+                                color="white"
+                                style={{position: 'absolute', bottom: 1}}
+                            />
+                        )}
+                    </View>
                 </View>
-            </View>
-            <View style={{width: '80%'}}>
-                <Pressable onPress={()=>{
-                    router.push({
-                        pathname: '/episodes/[id]',
-                        params: {
-                            id: episode.podcastEpisode.id,
-                        }
-                    })
-                }}>
-                    <Text style={{ color: 'white', wordWrap: "break", marginLeft: 10 }} numberOfLines={2}>{episode.podcastEpisode.name}</Text>
-                    <Text style={{ color: styles.whiteSubText, wordWrap: "break", marginLeft: 10 }} numberOfLines={2}>{episode.podcastEpisode.description}</Text>
-                </Pressable>
-                <View style={{marginLeft: 10, marginTop: 'auto', display: 'flex', flexDirection: 'row'}}>
-                    <ProgressCircle progress={totalProgressPercentage} />
-                    {remainingSeconds.alreadyPlaying ? <Text style={{color: 'white', marginTop: 'auto', marginBottom: 'auto'}}>{t('time-left', {time: remainingSeconds.time})}</Text>:
-                        <Text style={{color: 'white', marginTop: 'auto', marginBottom: 'auto'}}>{t('time', {time: remainingSeconds.time})}</Text>}
+                <View style={{width: '80%'}}>
+                    <Pressable
+                        onPress={() => {
+                            router.push({
+                                pathname: '/episodes/[id]',
+                                params: {id: episode.podcastEpisode.id}
+                            });
+                        }}
+                    >
+                        <Text style={{
+                            color: 'white',
+                            marginLeft: 10
+                        }} numberOfLines={2}>
+                            {episode.podcastEpisode.name}
+                        </Text>
+                        <Text style={{
+                            color: styles.whiteSubText,
+                            marginLeft: 10
+                        }} numberOfLines={2}>
+                            {episode.podcastEpisode.description}
+                        </Text>
+                    </Pressable>
+                    <View style={{
+                        marginLeft: 10,
+                        marginTop: 'auto',
+                        display: 'flex',
+                        flexDirection: 'row'
+                    }}>
+                        <ProgressCircle
+                            progress={totalProgressPercentage}
+                            episode={episode}
+                            onPlayPress={handlePlay}
+                        />
+                        <Text style={{
+                            color: 'white',
+                            marginTop: 'auto',
+                            marginBottom: 'auto'
+                        }}>
+                            {t(remainingSeconds.alreadyPlaying ? 'time-left' : 'time',
+                                {time: remainingSeconds.time})}
+                        </Text>
+                    </View>
                 </View>
             </View>
         </View>
-    </View>
-}
+    );
+};
