98805a5b0dda8b3c59af459b1cc47e86a65740db
SamTV12345 <samtv@github.noreply.com>
2026-04-03 22:42:31 +0200

    feat: only connect to backend after authentication with apikey

--- ui/src/routing/Root.tsx
+++ ui/src/routing/Root.tsx
@@ -1,15 +1,26 @@
 import { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-import useCommon from '../store/CommonSlice'
+import { Outlet } from 'react-router-dom'
 import App from '../App'
 import { AudioComponents } from '../components/AudioComponents'
 import { EpisodeSearchModal } from '../components/EpisodeSearchModal'
 import { Header } from '../components/Header'
 import { MainContentPanel } from '../components/MainContentPanel'
 import { Sidebar } from '../components/Sidebar'
+import { $api } from '../utils/http'
+import { connectSocket } from '../utils/socketio'
 
 
 export const Root = () => {
+    const user = $api.useQuery('get', '/api/v1/users/{username}', {
+        params: { path: { username: 'me' } }
+    })
+
+    useEffect(() => {
+        if (user.data) {
+            connectSocket(user.data.apiKey || '')
+        }
+    }, [user.data])
+
     return (
         <App>
             <div className="grid grid-cols-[1fr] md:grid-cols-[18rem_1fr] grid-rows-[1fr_auto]">
